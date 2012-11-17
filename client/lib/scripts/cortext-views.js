5fcbc91e2b033f2ba10b3b7f28a54f2f0da389ed
elishowk <elishowk@nonutc.fr>
2012-11-17 18:24:37 +0100

    comments

--- client/lib/scripts/cortext-views.js
+++ client/lib/scripts/cortext-views.js
@@ -5,24 +5,60 @@ if (Meteor.isClient) {
         }
         if (window.CorTextGraphs.sigmaview === undefined) {
             var SigmaView = Backbone.View.extend({
+                /*
+                 * Sigma instance
+                 */
                 sigma: null,
                 initialize: function() {
                     if (this.sigma !== null) {
                         this.sigma.emptyGraph();
                     }
                 },
+                /*
+                 * Graph drawing function
+                 */
                 pushGraph: function(object) {
+                    this._categories = {};
                     var that = this;
+                    /*
+                     * draw nodes
+                     */
                     object.nodes && _.keys(object.nodes).forEach(function(key) {
                         var node = object.nodes[key];
+                        /*
+                         * on the fly adding clusters
+                         */
+                        if (that._categories[node.community.toString()] === undefined) {
+                            that._categories[node.community.toString()] = {
+                                id: 'cluster-' + node.community.toString(),
+                                size: 2000,
+                                x: [node.x],
+                                y: [node.y],
+                                label: '',
+                            };
+                        } else {
+                            that._categories[node.community.toString()].x.push(node.x);
+                            that._categories[node.community.toString()].y.push(node.y);
+                        }
                         node.id = key;
                         node.size = node.weight;
-                        node.color = '#D37';
                         node.inDegree = node['in-degree'];
                         node.outDegree = node['out-degree'];
                         that.sigma.addNode(node.id, node);
                     });
-
+                    /* TODO draw clusters above nodes without hiding them all ....
+                     * this._categories && _.keys(this._categories).forEach(function(key) {
+                        var node = that._categories[key];
+                        node.x = _.reduce(node.x, function(memo, num) {
+                            return memo + num; }, 0) / node.x.length;
+                        node.y = _.reduce(node.y, function(memo, num) {
+                            return memo + num; }, 0) / node.y.length;
+                        that.sigma.addNode(node.id, node);
+                    });
+                    */
+                    /*
+                     * draw edges
+                     */
                     object.edges && _.keys(object.edges).forEach(function(key) {
                         var edge = object.edges[key];
                         validID = edge['source'] && edge['source'];
@@ -58,17 +94,12 @@ if (Meteor.isClient) {
                         });
                 }
             });
+            /*
+             * init view
+             */
             window.CorTextGraphs.sigmaview = new SigmaView({
                 el: $('#sigma').get(0)
             });
-
-            /* TODO use backbone layout for nested views
-             * new Backbone.Layout({
-                template: 'body',
-                views: {
-                    '#sigma': new SigmaView()
-                }
-            })*/
         }
     });
 }
