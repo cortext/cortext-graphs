29b0cb6c124ea385ae620b6aba5b2a84478242be
elishowk <elishowk@nonutc.fr>
2012-12-16 18:20:05 +0100

    removed template rendered function

--- client/lib/scripts/templates.js
+++ client/lib/scripts/templates.js
@@ -1,16 +1,7 @@
-
 if (Meteor.isClient) {
     Meteor.startup(function() {
         Session.set('selected_cluster', undefined);
         Session.set('selected_neighbors', undefined);
         Session.set('selected_node', undefined);
-        Template.nodepanel.rendered = function() {
-            $(this.findAll('.cluster')).editable({
-                type: 'textarea',
-                title: 'set a label for the cluster',
-                pk: 1,
-                enable: true
-            });
-        };
     });
 }
