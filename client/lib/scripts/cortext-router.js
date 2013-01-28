a2bd7c6061ebbd8f7fed972ccb63fd4fe2a5c63f
elishowk <elishowk@nonutc.fr>
2013-01-28 18:30:58 +0100

    title bar style fixed

--- client/lib/scripts/cortext-router.js
+++ client/lib/scripts/cortext-router.js
@@ -4,9 +4,13 @@ if (Meteor.isClient) {
         var rightlist = document.getElementById('navbar-right');
         var navbar = document.getElementById('navbar-fluid');
         navbar.insertBefore( Meteor.render(function () {
-            return '<a onclick="javascript:void(0)" class="brand"><small>'+
-                Session.get('title')
-                +'</small></a>';
+            if (Session.get('title')) {
+                return '<a onclick="javascript:void(0)" class="brand graph-title"><small class="graph-title-text">'+
+                    Session.get('title')
+                    +'</small></a>';
+            } else {
+                return "";
+            }
         }), rightlist );
 
         if (window.CorTextGraphs.MainRouter === undefined) {
