ba6b9b6d81722927d6d3641bd97c8fd8471d1377
duong tam kien <tk@deveha.com>
2013-06-20 15:32:20 +0200

    cleaning

--- client/cortext-router.js
+++ client/cortext-router.js
@@ -1,43 +1,41 @@
 'use strict';
-if (Meteor.isClient) {
-    Meteor.startup(function() {
 
-        var rightlist = document.getElementById('navbar-right');
-        var navbar = document.getElementById('navbar-fluid');
-        navbar.insertBefore( Meteor.render(function () {
-            if (Session.get('title')) {
-                return '<a onclick="javascript:void(0)" class="brand graph-title"><small class="graph-title-text">'+
-                    Session.get('title')
-                    +'</small></a>';
-            } else {
-                return "";
-            }
-        }), rightlist );
+Meteor.startup(function() {
 
-        if (window.CorTextGraphs.MainRouter === undefined) {
-            window.CorTextGraphs.MainRouter = Backbone.Router.extend({
-                routes: {
-                    '': 'default',
-                    'open/:path/*clusterpath?node=:nodeid': 'index',
-                    'open/:path/*clusterpath?node=:nodeid/edit': 'edit'
-                },
-                default: function() {
-                    Session.set('title', '');
-                    $('#sigma').html(Template.hello(
-                        {example: true, text: 'Welcome to CorText Graphs'}));
-                },
-                index: function(path, clusterpath, nodeid) {
-                    Session.set('path', decodeURIComponent(path));
-                    Session.set('clusterpath', decodeURIComponent(clusterpath));
-                    window.CorTextGraphs.sigmaview.render(nodeid);
-                },
-                edit: function(path, clusterpath, nodeid) {
-                    Session.set('path', decodeURIComponent(path));
-                    Session.set('clusterpath', decodeURIComponent(clusterpath));
-                    window.CorTextGraphs.noteEdit.render(nodeid);
-                }
-            });
+    var rightlist = document.getElementById('navbar-right');
+    var navbar = document.getElementById('navbar-fluid');
+    navbar.insertBefore( Meteor.render(function () {
+        if (Session.get('title')) {
+            return '<a onclick="javascript:void(0)" class="brand graph-title"><small class="graph-title-text">'+
+                Session.get('title')
+                +'</small></a>';
+        } else {
+            return "";
         }
-    });
-}
+    }), rightlist );
 
+    if (window.CorTextGraphs.MainRouter === undefined) {
+        window.CorTextGraphs.MainRouter = Backbone.Router.extend({
+            routes: {
+                '': 'default',
+                'open/:path/*clusterpath?node=:nodeid': 'index',
+                'open/:path/*clusterpath?node=:nodeid/edit': 'edit'
+            },
+            default: function() {
+                Session.set('title', '');
+                $('#sigma').html(Template.hello(
+                    {example: true, text: 'Welcome to CorText Graphs'}));
+            },
+            index: function(path, clusterpath, nodeid) {
+                Session.set('path', decodeURIComponent(path));
+                Session.set('clusterpath', decodeURIComponent(clusterpath));
+                window.CorTextGraphs.sigmaview.render(nodeid);
+            },
+            edit: function(path, clusterpath, nodeid) {
+                Session.set('path', decodeURIComponent(path));
+                Session.set('clusterpath', decodeURIComponent(clusterpath));
+                window.CorTextGraphs.noteEdit.render(nodeid);
+            }
+        });
+    }
+});
